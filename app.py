e3afaca3790c808bca0d071a8b63a3a509e7105b
Rif'at Ahdi R <10791791+atrifat@users.noreply.github.com>
2024-06-25 02:14:02 +0000

    feat: support custom and hybrid model

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import pandas as pd
 from flask import Flask, request, jsonify
 import functools
 import datetime
-import torch
 from detoxify import Detoxify
 import logging
+import torch
 from flask_caching import Cache
+import pickle
 
 load_dotenv()
 
@@ -16,11 +17,27 @@ API_TOKEN = os.getenv("API_TOKEN", "")
 APP_ENV = os.getenv("APP_ENV", "production")
 LISTEN_HOST = os.getenv("LISTEN_HOST", "0.0.0.0")
 LISTEN_PORT = os.getenv("LISTEN_PORT", "7860")
+
+CUSTOM_MODEL_PATH = os.getenv(
+    "CUSTOM_MODEL_PATH",
+    os.path.dirname(os.path.abspath(__file__))
+    + "/experiments/model_voting_partial_best.pkl",
+)
+CUSTOM_VECTORIZER_PATH = os.getenv(
+    "CUSTOM_VECTORIZER_PATH",
+    os.path.dirname(os.path.abspath(__file__))
+    + "/experiments/vectorizer_count_no_stop_words.pkl",
+)
+
 DETOXIFY_MODEL = os.getenv("DETOXIFY_MODEL", "unbiased-small")
+HATE_SPEECH_MODEL = os.getenv("HATE_SPEECH_MODEL", "detoxify")
 CACHE_DURATION_SECONDS = int(os.getenv("CACHE_DURATION_SECONDS", 60))
 ENABLE_CACHE = os.getenv("ENABLE_CACHE", "false") == "true"
+POTENTIAL_TOXIC_WORDS = list(
+    filter(None, os.getenv("POTENTIAL_TOXIC_WORDS", "").split(","))
+)
+HYBRID_THRESOLD_CHECK = float(os.getenv("HYBRID_THRESOLD_CHECK", 0.5))
 TORCH_DEVICE = os.getenv("TORCH_DEVICE", "auto")
-
 APP_VERSION = "0.2.0"
 
 # Setup logging configuration
@@ -47,7 +64,22 @@ if TORCH_DEVICE == "auto":
 else:
     torch_device = TORCH_DEVICE
 
-model = Detoxify(DETOXIFY_MODEL, device=torch_device)
+if HATE_SPEECH_MODEL in ["hybrid", "detoxify"]:
+    model = Detoxify(DETOXIFY_MODEL, device=torch_device)
+
+if HATE_SPEECH_MODEL in ["hybrid", "custom"]:
+    try:
+        with open(CUSTOM_VECTORIZER_PATH, "rb") as f:
+            vectorizer = pickle.load(f)
+    except Exception as e:
+        vectorizer = None
+
+    try:
+        with open(CUSTOM_MODEL_PATH, "rb") as f:
+            model_custom = pickle.load(f)
+    except Exception as e:
+        raise e
+
 
 app = Flask(__name__)
 
@@ -107,6 +139,55 @@ def perform_hate_speech_analysis(query):
     return result
 
 
+def perform_hate_speech_analysis_custom(query):
+    query_vector = vectorizer.transform([query]) if vectorizer != None else ["query"]
+
+    result = {
+        "identity_attack": 0.0,
+        "insult": 0.0,
+        "obscene": 0.0,
+        "severe_toxicity": 0.0,
+        "sexual_explicit": 0.0,
+        "threat": 0.0,
+        "toxicity": 0.0,
+    }
+
+    temp_result = model_custom.predict_proba(query_vector)
+    result["toxicity"] = temp_result[0][1].round(3).astype("float")
+
+    return result
+
+
+def perform_hate_speech_analysis_hybrid(
+    query, thresold_check=0.5, potential_toxic_words=[]
+):
+    result = {
+        "identity_attack": 0.0,
+        "insult": 0.0,
+        "obscene": 0.0,
+        "severe_toxicity": 0.0,
+        "sexual_explicit": 0.0,
+        "threat": 0.0,
+        "toxicity": 0.0,
+    }
+    temp_result_custom = perform_hate_speech_analysis_custom(query)
+
+    has_potential_toxic_word = False
+    for word in potential_toxic_words:
+        if word in query:
+            has_potential_toxic_word = True
+            break
+
+    if temp_result_custom["toxicity"] > thresold_check or has_potential_toxic_word:
+        temp_result_detoxify = perform_hate_speech_analysis(query)
+        if temp_result_detoxify["toxicity"] > thresold_check:
+            result = temp_result_detoxify
+    else:
+        result = temp_result_custom
+
+    return result
+
+
 @app.errorhandler(Exception)
 def handle_exception(error):
     res = {"error": str(error)}
@@ -120,7 +201,16 @@ def predict():
     data = request.json
     q = data["q"]
     start_time = datetime.datetime.now()
-    result = perform_hate_speech_analysis(q)
+
+    if HATE_SPEECH_MODEL == "custom":
+        result = perform_hate_speech_analysis_custom(q)
+    elif HATE_SPEECH_MODEL == "hybrid":
+        result = perform_hate_speech_analysis_hybrid(
+            q, HYBRID_THRESOLD_CHECK, POTENTIAL_TOXIC_WORDS
+        )
+    else:
+        result = perform_hate_speech_analysis(q)
+
     end_time = datetime.datetime.now()
     elapsed_time = end_time - start_time
     logging.debug("elapsed predict time: %s", str(elapsed_time))
